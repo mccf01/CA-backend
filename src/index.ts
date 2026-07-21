ee415fcba2765e4bc5202886b9cd0ede3af917c6
Matheus Franco <matheusccfranco02@gmail.com>
2026-07-21 17:47:26 -0300

    refac: Mudanca de porta do servidor para não entrar em conflito com a do Postgres. Mudancas feitas pelo Code Review automatizado

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./server";
 
-const PORT = process.env.PORT || 5432
+const PORT = process.env.PORT || 3333
 
 app.listen(PORT, () => {
     console.info(`✅️SERVER IS RUNNING ON PORT ${PORT}`);
